087b2b1a2aedab682a9a235eda06d2e1b2df9871
cyamauch <cyamauch@ir.isas.jaxa.jp>
2020-08-30 19:39:32 +0900

    Updated make_output_filename.cc: Improved code for create new filename.

--- src/make_output_filename.cc
+++ src/make_output_filename.cc
@@ -12,11 +12,14 @@ static int make_output_filename( const char *_filename_in,
     if ( 0 <= pos_dot ) filename_in.copy(0, pos_dot, filename_out);
     else filename_in.copy(filename_out);
 
-    if ( filename_in.findf(".%s.",bit_str) < 0 ) {
-	filename_out->appendf(".%s.%s.tiff",bit_str,appended_str);
+    if ( 0 <= filename_in.findf(".%s.",bit_str) ) {
+	filename_out->appendf(".%s.tiff",appended_str);
+    }
+    else if ( 0 <= filename_in.findf("_%s.",bit_str) ) {
+	filename_out->appendf(".%s.tiff",appended_str);
     }
     else {
-	filename_out->appendf(".%s.tiff",appended_str);
+	filename_out->appendf(".%s.%s.tiff",bit_str,appended_str);
     }
 
     return 0;
