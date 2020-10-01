461187ae441b6a1815c97706d11cb5f223f09a85
cyamauch <cyamauch@ir.isas.jaxa.jp>
2020-10-02 00:12:37 +0900

    Updated cygwin_installer/setup64_develtools.bat: Appended X11 packages.

--- src/cygwin_installer/setup_develtools.bat
+++ src/cygwin_installer/setup_develtools.bat
@@ -5,6 +5,7 @@ rem Install additional packages
 C:
 chdir C:\cygwin
 setup-x86.exe --packages ncurses,wget,zip,gcc-core,gcc-g++,libisl10,libisl-devel,libcloog-isl-devel,libncurses-devel,libreadline-devel,libtiff-devel,make,emacs,emacs-anthy,emacs-el,emacs-w32,emacs-X11 --quiet-mode
+setup-x86.exe --packages xorg-server,xorg-server-common,xorg-x11-fonts-misc,xorg-x11-fonts-dpi75,xorg-x11-fonts-dpi100,xauth,xterm,xset,xmag,xkill,xinit --quiet-mode
 
 
 rem Create home directory
